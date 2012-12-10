5a1e43e0d83b3995a33053cd3a859bcc8561b84b
Masahito Yoshida <masahito@axsh.net>
2012-12-11 00:58:09 +0900

    plugin/copyfile: enable to define multi directory.

--- plugins-available/copyfile.sh
+++ plugins-available/copyfile.sh
@@ -1,19 +1,22 @@
 function list_guestroot_tree() {
-  local guestroot_dir=$(pwd)/guestroot
-  [[ -d "${guestroot_dir}" ]] || return 0
+  local dir=$1
 
-  cd ${guestroot_dir}
+  [[ -d "${dir}" ]] || return 0
+  cd ${dir}
   find . ! -type d | sed s,^\.,, | egrep -v '^/(.gitkeep|functions.sh)'
 }
 
 function generate_copyfile() {
-  local guestroot_dir=$(pwd)/guestroot
-  [[ -d "${guestroot_dir}" ]] || return 0
+  local guestroot_dirs="${guestroot_dir:-$(pwd)/guestroot}"
 
   echo "[INFO](copyfile) Generating copy.txt"
-  while read line; do
-    echo ${guestroot_dir}${line} ${line}
-  done < <(list_guestroot_tree) > $(pwd)/copy.txt
+  local dir=
+  for dir in ${guestroot_dirs}; do
+    local line=
+    while read line; do
+      echo ${dir}${line} ${line}
+    done < <(list_guestroot_tree ${dir})
+  done > $(pwd)/copy.txt
   cat $(pwd)/copy.txt
 }
 
